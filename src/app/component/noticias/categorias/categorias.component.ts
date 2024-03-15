6a20ae41ffcd2f047a20b5b41103110d7920b8d5
Denis <denisjoelcif@gmail.com>
2024-03-14 19:27:21 -0600

    Mostrar categorias y metodo para filtrar

--- src/app/component/noticias/categorias/categorias.component.ts
+++ src/app/component/noticias/categorias/categorias.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { categoriaDTO } from 'src/app/dto/Categoria.dto';
+import { CategoriaService } from 'src/app/service/categoria/categoria.service';
 
 @Component({
   selector: 'app-categorias',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent {
+  categorias: categoriaDTO[] | undefined;
 
+  constructor(private categoriaService: CategoriaService) { }
+
+  ngOnInit(): void {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias(): void {
+    this.categoriaService.listarCategorias()
+      .subscribe(categorias => {
+        this.categorias = categorias;
+      });
+  }
+
+  guardarIdCategoria(idCategoria: number): void {
+    sessionStorage.setItem("idCategoria", idCategoria.toString());
+    window.location.reload();
+  }
+
+  quitarFiltro() {
+    sessionStorage.removeItem("idCategoria")
+    window.location.reload();
+  }
 }
