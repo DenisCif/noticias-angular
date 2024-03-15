45cf19365fbeb6d54e3f760ff1681ed2c2a6b109
Denis <denisjoelcif@gmail.com>
2024-03-14 19:02:27 -0600

    Noticas Service

--- src/app/service/noticia/noticia.service.ts
+++ src/app/service/noticia/noticia.service.ts
@@ -1,9 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NoticiaDTO } from 'src/app/dto/Noticia.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/noticia';
+  private token = sessionStorage.getItem('token');
+  private idUsuario = sessionStorage.getItem('idUsuario');
+
+  constructor(private http: HttpClient) { }
+
+  listarNoticias(): Observable<NoticiaDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this.http.get<NoticiaDTO[]>(this.baseUrl + '/listado?idUsuario=' + this.idUsuario, { headers: headers });
+  }
+
+  listarNoticiasPorCategoria(porCategoria: boolean, idCategoria: number): Observable<NoticiaDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this.http.get<NoticiaDTO[]>(this.baseUrl +
+      '/listado?porCategoria=' + porCategoria + '&idCategoria=' + idCategoria + '&idUsuario'
+      + this.idUsuario, { headers: headers });
+  }
+
+  verNoticia(idNoticia: number): Observable<NoticiaDTO> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+    return this.http.get<NoticiaDTO>(this.baseUrl + '/ver/' + idNoticia + '?idUsuario=' + this.idUsuario, { headers: headers });
+  }
+
+  listarNoticiasRelacionadas(idNoticia: number): Observable<NoticiaDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this.http.get<NoticiaDTO[]>(this.baseUrl + '/' + idNoticia + '/relacionadas?=idUsuario=' + this.idUsuario, { headers: headers });
+  }
+
 }
