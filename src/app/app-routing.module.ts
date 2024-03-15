ecd0795260224c16deac08816087d96a6ff9d85b
Denis <denisjoelcif@gmail.com>
2024-03-14 19:12:57 -0600

    Creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './component/inicio/inicio/inicio.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { RegisterComponent } from './component/auth/register/register.component';
+import { DetalleNoticiaComponent } from './component/noticias/detalle-noticia/detalle-noticia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'detalle', component: DetalleNoticiaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
