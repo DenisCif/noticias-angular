704c8ea760a9b5f7d9b23eac256ec71e1dddecd6
Denis <denisjoelcif@gmail.com>
2024-03-14 19:33:00 -0600

    Ver detalle de noticia

--- src/app/component/noticias/detalle-noticia/detalle-noticia.component.ts
+++ src/app/component/noticias/detalle-noticia/detalle-noticia.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NoticiaDTO } from 'src/app/dto/Noticia.dto';
+import { NoticiaService } from 'src/app/service/noticia/noticia.service';
 
 @Component({
   selector: 'app-detalle-noticia',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalle-noticia.component.css']
 })
 export class DetalleNoticiaComponent {
+  idNoticia: number = 0;
+  noticia: NoticiaDTO | undefined;
+  noticias: NoticiaDTO[] | undefined;
+
+
+  constructor(private route: ActivatedRoute, private noticiaService: NoticiaService) { }
+
+  ngOnInit(): void {
+    this.obtenerIDNoticia();
+    this.cargarNoticia();
+  }
+
+  cargarNoticia(): void {
+    this.noticiaService.verNoticia(this.idNoticia)
+      .subscribe(noticia => {
+        this.noticia = noticia;
+      });
+  }
+  obtenerIDNoticia() {
+    const idNoticiaString = sessionStorage.getItem("idNoticia");
+    if (idNoticiaString) {
+      this.idNoticia = parseInt(idNoticiaString, 10);
+    }
+  }
+
 
 }
