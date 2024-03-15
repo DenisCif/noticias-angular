5df1ec0b249d70dafeb57802f07248cc220dd00e
Denis <denisjoelcif@gmail.com>
2024-03-14 18:54:22 -0600

    authService

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/auth';
+  private tokenKey = 'token';
+  private refreshToken = 'refreshToken';
+  private idUsuario = 'idUsuario';
+  private message = 'message';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(email: string, password: string): Observable<any> {
+    const formData = {
+      email: email,
+      password: password
+    };
+
+    return this.http.post(`${this.apiUrl}/signin`, formData)
+      .pipe(
+        map((response: any) => {
+          sessionStorage.setItem(this.tokenKey, response.token);
+          sessionStorage.setItem(this.refreshToken, response.refreshToken);
+          sessionStorage.setItem(this.idUsuario, response.idUsuario);
+          sessionStorage.setItem(this.message, response.message);
+          this.router.navigate(['/']);
+          return response;
+        })
+      );
+  }
+
+  signup(nombre: string, apellido: string, email: string, password: string): Observable<any> {
+    const formData = {
+      nombre: nombre,
+      apellido: apellido,
+      email: email,
+      password: password
+    };
+    return this.http.post(`${this.apiUrl}/signup`, formData)
+  }
+
+  logout(): void {
+    sessionStorage.clear();
+    this.router.navigate(['/signin']);
+  }
+
 }
