beee48dc366e52d9162121c712faba1a590f4b01
Denis <denisjoelcif@gmail.com>
2024-03-14 19:34:43 -0600

    instanciando metodo para ver noticias relacionadas

--- src/app/component/noticias/noticias-relacionadas/noticias-relacionadas.component.ts
+++ src/app/component/noticias/noticias-relacionadas/noticias-relacionadas.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NoticiaDTO } from 'src/app/dto/Noticia.dto';
+import { NoticiaService } from 'src/app/service/noticia/noticia.service';
 
 @Component({
   selector: 'app-noticias-relacionadas',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./noticias-relacionadas.component.css']
 })
 export class NoticiasRelacionadasComponent {
+  noticias: NoticiaDTO[] | undefined;
+  idNoticia: number = 0;
+
+  constructor(private noticiasSerice: NoticiaService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.obtenerIDNoticia();
+    this.obtenerNoticiasRelacionadas();
+  }
+
+  obtenerNoticiasRelacionadas(): void {
+    if (this.idNoticia) {
+      this.noticiasSerice.listarNoticiasRelacionadas(this.idNoticia)
+        .subscribe(noticias => {
+          this.noticias = noticias;
+        });
+    }
+  }
+
+  obtenerIDNoticia() {
+    const idNoticiaString = sessionStorage.getItem("idNoticia");
+    if (idNoticiaString) {
+      this.idNoticia = parseInt(idNoticiaString, 10);
+    }
+  }
+
+
+  verDetalles(idNoticia: number): void {
+    sessionStorage.setItem("idNoticia", idNoticia.toString());
+    this.router.navigate(['/detalle']).then(() => {
+      window.location.reload();
+    });
+  }
 
 }
